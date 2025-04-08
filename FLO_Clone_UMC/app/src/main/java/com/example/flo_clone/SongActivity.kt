6b1f47cb2114a4092cf085e44543f8367afc1d58
Lee-CH810 <2002ckdghks@naver.com>
2025-04-08 23:17:11 +0900

    SongActivity 재생, 정지 버튼 바꾸기

--- FLO_Clone_UMC/app/src/main/java/com/example/flo_clone/SongActivity.kt
+++ FLO_Clone_UMC/app/src/main/java/com/example/flo_clone/SongActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flo_clone
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo_clone.databinding.ActivitySongBinding
 
@@ -25,6 +26,34 @@ class SongActivity : AppCompatActivity() {
         // 이전에 출력 중이던 Fragment가 조합된 activity_main.xml의 화면을 볼 수 있음.
         binding.songDownIb.setOnClickListener {
             finish()
+        }
+
+        // 음악이 재생 및 정지 중일때, 재생 버튼의 변화 처리
+        binding.songMiniplayerIv.setOnClickListener {
+            // 음악이 정지 중일 때, 재생 버튼 띄움
+            setPlayerStatus(false)
+        }
+        binding.songPauseIv.setOnClickListener {
+            // 음악이 재생 중일 때 정지 버튼 띄움
+            setPlayerStatus(true)
+        }
+    }
+
+
+    /**
+     * 재생 중이면, 정지 버튼.
+     * 정지 중이면, 재생 버튼을 띄우는 함수
+     * @param isPlaying 현재 재생 중인지의 여부
+     */
+    fun setPlayerStatus (isPlaying: Boolean) {
+        if (isPlaying) {
+            // 재생 중
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
+        } else {
+            // 정지 중
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
         }
     }
 }
