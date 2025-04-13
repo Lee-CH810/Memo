1aa3e6719157af8b430fbb25c4af34cc74d39568
Lee-CH810 <2002ckdghks@naver.com>
2025-04-14 01:06:40 +0900

    Timer 완성 Issue: Thread 중첩 생성을 피하기 위해 myTimer.start()를 onCreate에서 한 번만 실행함

--- FLO_Clone_UMC/timer/src/main/java/com/example/timer/MainActivity.kt
+++ FLO_Clone_UMC/timer/src/main/java/com/example/timer/MainActivity.kt
@@ -15,12 +15,20 @@ import java.util.Timer
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * 1. 변수 설정
+     */
 
+    // 바인딩 객체
     lateinit var binding : ActivityMainBinding
+    // 타이머를 수행할 스레드 객체
+    var myTimer: MyTimer = MyTimer()
 
-    lateinit var myTimer: MyTimer
-
-    var isStart:Boolean = false
+    // 상태
+    // isStart: 현재 start 상태인지
+    // isClear: Clear 버튼이 눌린 상태인지
+    var isStart: Boolean = false
+    var isClear: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,27 +36,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        myTimer.start() // 타이머 스레드 생성
+
         /**
+         * 버튼 전환
          * Start -> Pause
          * Pause -> Start
          */
         binding.timerStartB.setOnClickListener {
-            Log.d("start", "타이머 시작")
+            // 타이머 실행
+            Log.d("Status", "Start: 타이머 시작")
+
             // Start이면 true -> Pause로
             setButton(true)
-            startTimer() // timer 객체 생성 및 타이머 실행
         }
         binding.timerPauseB.setOnClickListener {
-            Log.d("pause", "타이머 정지")
+            // 타이머 정지
+            Log.d("Status", "Pause: 타이머 정지")
+
             // Pasue이면 false -> Start로
             setButton(false)
         }
-
-        /**
-         * Clear 처리
-         */
         binding.timerClearB.setOnClickListener {
-            reset()
+            // 타이머 초기화
+            Log.d("Status", "Clear: 타이머 초기화")
+
+            setButton(false) // Button을 Pause로 바꿈
+            isClear = true // Clear 누른 상태
         }
 
     }
@@ -58,33 +72,30 @@ class MainActivity : AppCompatActivity() {
      * @param isStart 현재 Start된 상태인지
      */
     private fun setButton(isStart: Boolean) {
+        // setButton을 호출한 곳에서 준 isStart 값을 전역변수에 반영
         this.isStart = isStart
         Log.d("isStart", "isStart: $isStart")
+
         if (isStart) {
             // Start 버튼 누르면
-            Log.d("Start/Pause", "start -> pause")
+            Log.d("Button", "start -> pause")
             binding.timerStartB.visibility = View.GONE
-            binding.timerPauseB.visibility = View.VISIBLE
+            binding.timerPauseB.visibility = View.VISIBLE // Pause가 보이도록
         } else {
             // Pause 버튼 누르면
-            Log.d("Start/Pause", "pause -> start")
+            Log.d("Button", "pause -> start")
             binding.timerPauseB.visibility = View.GONE
-            binding.timerStartB.visibility = View.VISIBLE
+            binding.timerStartB.visibility = View.VISIBLE // Start가 보이도록
         }
     }
 
-    private fun startTimer() {
-        myTimer = MyTimer()
-        myTimer.start()
-    }
-
-    private fun reset() {
-        myTimer.interrupt()
-    }
-
+    /**
+     * 타이머를 실행할 Thread 객체
+     * 주의할 점: Thread가 실행 중인 것과 시간이 카운트되는 것은 다른 개념이라는 것을 유의
+     */
     inner class MyTimer() : Thread() {
-        var mills: Int = 0 // 밀리세컨드
-        var second: Int = 0 // 초
+        var mills: Int = 0
+        var second: Int = 0
 
         override fun run() {
             super.run()
@@ -92,17 +103,27 @@ class MainActivity : AppCompatActivity() {
             while(true) {
                 // 현재 Start 중이면 시간 흐름
                 if (isStart) {
-//                    Log.d("timer", "timer 작동 중")
-                    sleep(50)
-                    mills += 50 // 밀리세컨드++
+                    sleep(10)
+                    mills += 10 // 밀리세컨드++
                     if (mills % 1000 == 0) {
                         second++
                         mills = 0
                     }
                     runOnUiThread {
-                        binding.timerTimeTv.text = String.format("%02d:%02d.%2d", second / 60, second % 60, mills)
+                        binding.timerTimeTv.text = String.format("%02d:%02d.%2d", second / 60, second % 60, mills / 10)
                     }
                     Log.d("Time", "second: $second, mills: $mills")
+                }
+                // Clear가 눌렸는지 확인
+                // Clear 세팅 함수를 따로 빼주려 했지만, 해당 작업을 class 외부에서 수행하면
+                // Thread 내의 while이 한 번 더 실행되는 이슈가 발생해서 run() 내부에 작성했음
+                if (isClear) {
+                    mills = 0
+                    second = 0
+                    runOnUiThread {
+                        binding.timerTimeTv.text = "00:00:00"
+                    }
+                    isClear = false // 다시 Clear가 눌리지 않은 것처럼 설정
                 }
             }
         }
@@ -113,8 +134,12 @@ class MainActivity : AppCompatActivity() {
  * 현재 clear의 수행을 위해서 interrupt를 걸어주었는데, clear 시 앱이 아예 튕김
  * clear에 interrupt 걸어준 것으로 인해 start를 눌렀을 시 Thread를 생성하게 하였더니, start와 pause를 누를 때마다 Thread가 하나씩 추가되는 문제 발생
  * --> Thread를 죽이는 방법 OR Thread 정지 방법
+ * --> Thread가 실행 중이라고 시간도 카운트되는 것이 아니고, Thread는 그냥 또다른 작업 라인이고 그곳에서 시간 카운트라는 작업을 하는 것임을 이해하면
+ * 굳이 Thread 죽이고 할 필요 없음.
  */
+
 /**
  * 방법 2) Data Class로 Timer의 구성을 second, mills로 구성하고
  * clear 시 Timer.second = 0, Timer.mills = 0으로 만드는 방법이 있음.
+ * --> 오류가 더 잦음. 컴파일 에러가 아닌, 런타임 에러로 의도한 대로, 의도한 값으로 흘러가지 않는 부분이 많았음
  */
